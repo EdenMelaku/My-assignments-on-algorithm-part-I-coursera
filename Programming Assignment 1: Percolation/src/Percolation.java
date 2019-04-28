e4097d17fc008b44c812b15e5f107bee0b412589
eden melaku <edenzzzmelaku@gmail.com>
2019-04-28 16:45:31 +0300

    starting assignment 1 (percolation)

---
+++ Programming Assignment 1: Percolation/src/Percolation.java
@@ -0,0 +1,53 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class Percolation {
+	
+	public int[][] grid;
+	   public Percolation(int n)                // create n-by-n grid, with all sites blocked\
+	   
+	   {
+	   grid=new int[n][n];
+	   for (int i=0;i<n;i++) {
+		   for (int j=0;j<n;j++) {
+			   grid[i][j]=0;
+		   }
+	   }
+
+
+       }
+	   public    void open(int row, int col)    // open site (row, col) if it is not open already
+	   {
+		   grid[row][col]=1; 
+	   }
+	   public boolean isOpen(int row, int col)  // is site (row, col) open?
+	   {
+		if ( grid[row][col]==1) {
+			return true;
+		}   
+		else {
+			return false;
+		}
+	   }
+	   public boolean isFull(int row, int col)  // is site (row, col) full?
+	   {
+		   
+	   }
+	   public     int numberOfOpenSites()       // number of open sites
+	   {
+		   
+	   }
+	   public boolean percolates()              // does the system percolate?
+	   {
+		   
+	   }
+
+	   public static void main(String[] args)   // test client (optional)
+	   
+	   {
+		   
+		   
+	   }
+	}
