8542b2d65676c3a0a2ae7887a40fe5c173525141
eden melaku <edenzzzmelaku@gmail.com>
2019-05-04 14:38:51 +0300

    editing sugessted by the autograder

--- Programming Assignment 1: Percolation/src/Percolation.java
+++ Programming Assignment 1: Percolation/src/Percolation.java
@@ -2,11 +2,11 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-
 public class Percolation {
 	
-	public int[][] grid;
-	   public Percolation(int n)                // create n-by-n grid, with all sites blocked\
+	private int[][] grid;
+	   public Percolation(int n)                
+	   // create n-by-n grid, with all sites blocked\
 	   
 	   {
 	   grid=new int[n][n];
@@ -17,12 +17,15 @@ public class Percolation {
 	   }
 
 
+
        }
-	   public    void open(int row, int col)    // open site (row, col) if it is not open already
+	   public    void open(int row, int col)    
+	   // open site (row, col) if it is not open already
 	   {
 		   grid[row][col]=1; 
 	   }
-	   public boolean isOpen(int row, int col)  // is site (row, col) open?
+	   public boolean isOpen(int row, int col) 
+	    // is site (row, col) open?
 	   {
 		if ( grid[row][col]==1) {
 			return true;
@@ -33,6 +36,7 @@ public class Percolation {
 	   }
 	   public boolean isFull(int row, int col)  // is site (row, col) full?
 	   {
+		   try {
 		   if (grid[row-1][col]==1) {
 			   return true;
 		   }
@@ -46,6 +50,11 @@ public class Percolation {
 			   return true;
 		   }
 		   else {
+			   return false;
+		   }
+		   }
+		   catch(java.lang.IllegalArgumentException x) {
+			   System.out.println(x);
 			   return false;
 		   }
 		   
@@ -66,7 +75,9 @@ public class Percolation {
 	   }
 	   public boolean percolates()              // does the system percolate?
 	   {
+		   int Startingpoints=grid.length;
 		   
+		   return true;
 	   }
 
 	   public static void main(String[] args)   // test client (optional)
