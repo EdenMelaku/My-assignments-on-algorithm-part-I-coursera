9365a7d607f0291bbe8250590ced706867ff13a5
eden melaku <edenzzzmelaku@gmail.com>
2019-06-04 23:14:01 +0300

    most of the implementations are changed but stil got a lot to fix

--- ProgAssignment3/src/BruteCollinearPoints.java
+++ ProgAssignment3/src/BruteCollinearPoints.java
@@ -1,21 +1,149 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BruteCollinearPoints {
-    public BruteCollinearPoints(Point[] points)  {
 
+   private  Point [] points = null;
+   private LineSegment[] segments = null;
+    public BruteCollinearPoints(Point[] points) {
+
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        this.points = points;
+        Arrays.sort(points);
+
+
+        ArrayList<LineSegment> l = new ArrayList<LineSegment>();
+
+        double slopes[] = new double[points.length];
+        boolean isCol = true;
+        double curr;
+        for (int i = 0; i < points.length-1; i++) {
+            curr = slopes[0];
+            slopes[i] = points[0].slopeTo(points[i+1]);
+            if (curr != slopes[i]) {
+             isCol = false;
+            }
+
+           // System.out.print(slopes[i]+" ");
+        }
+       //  System.out.println();
+        if (isCol) {
+
+            LineSegment li = new LineSegment(points[0], points[points.length-1]);
+
+           // System.out.println(points[0]+" =>"+points[points.length-1]);
+
+            l.add(li);
+            segments = l.toArray(new LineSegment[l.size()]);
+            return;
+
+
+        } else {
+            double[][] s = new double[points.length][points.length];
+            for (int i = 0; i < points.length; i++) {
+                for (int j = i; j < points.length; j++) {
+                    s[i][j] = points[i].slopeTo(points[j]);
+                    s[j][i] = points[i].slopeTo(points[j]);
+
+                }
+            }
+
+            for (int k = 0; k < s.length; k++) {
+                for (int j = k; j < s.length; j++) {
+                    for (int y = j + 1; y < s.length; y++) {
+                        if (s[k][j] == s[k][y]) {
+
+                            LineSegment line = new LineSegment(points[k], points[y]);
+                        //    System.out.println(points[k]+" =>"+points[y]);
+
+                            if (!l.contains(line)) {
+                                l.add(line);
+                            }
+                        }
+                    }
+                }
+
+            }
+            segments = l.toArray(new LineSegment[l.size()]);
+
+/*
+            for (int i = 0; i < s.length; i++) {
+                for (int j = 0;j < s.length; j++) {
+                    System.out.print(s[i][j] + " | ");
+                }
+                System.out.println();
+            }
+*/
+
+        }
 
 
 
     }  // finds all line segments containing 4 points
-    public int numberOfSegments(){
 
-return 0;
+    public int numberOfSegments() {
+
+     return segments.length;
 
     }        // the number of line segments
     public LineSegment[] segments()    {
 
-    return null;
+     return segments;
 
     }            // the line segments
+
+
+    public static void main(String... args) {
+
+        Point p = new Point(2, 4);
+        Point q = new Point(4, 6);
+        Point r = new Point(8, 10);
+        Point s = new Point(2, 0);
+
+        Point[] points = new Point[4];
+        points[0] = p;
+        points[1] = q;
+        points[2] = r;
+        points[3] = s;
+
+        //BruteCollinearPoints b = new BruteCollinearPoints(points);
+
+        //LineSegment[] l = b.segments();
+
+
+
+/*
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] point = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            point[i] = new Point(x, y);
+        }
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point x : point) {
+            x.draw();
+        }
+        StdDraw.show();
+ */
+
+
+    }
+
+
+
+
+
 }
 
